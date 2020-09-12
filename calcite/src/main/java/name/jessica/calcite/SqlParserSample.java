a26f2b04571a9df45236cc4ae08dffd57f5007a4
jessica <604284275@qq.com>
2020-09-12 18:07:06 +0800

    [Calcite-1][demo] calcite test

--- calcite/src/main/java/name/jessica/calcite/SqlParserSample.java
+++ calcite/src/main/java/name/jessica/calcite/SqlParserSample.java
@@ -9,7 +9,7 @@ import org.apache.calcite.sql.parser.SqlParser;
 public class SqlParserSample {
 	public static void main(String[] args) throws SqlParseException {
 		// Sql语句
-		String sql = "select * from emps where id = 1";
+		String sql = "select * from tables where id = 1";
 		// 解析配置
 		SqlParser.Config javaConfig = SqlParser.configBuilder().setLex(Lex.JAVA).build();
 		// 创建解析器
